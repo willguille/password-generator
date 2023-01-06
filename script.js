b9908694b7a921a9c62ada7a90652c0df3884e3d
wilguille <willyguille97@gmail.com>
2023-01-06 15:09:16 +0000

    Added code to script.js

--- script.js
+++ script.js
@@ -90,6 +90,29 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // Prompt the user for password length
+  var passwordLength = prompt("Enter the desired password length (must be between 10 and 64 characters):");
+
+  // Validate password length
+  while (passwordLength < 10 || passwordLength > 64) {
+    alert("Error: Password length must be between 10 and 64 characters. Please try again")
+    passwordLength = prompt("Enter the desired password length (must be between 10 and 64 characters):");
+  }
+
+  // Prompt the user for character types to include in password
+  var lowercase = confirm("Include lowercase characters in the password?");
+  var uppercase = confirm("Include uppercase characters in the password?");
+  var numeric = confirm("Include numeric characters in the password?");
+  var special = confirm("Include special characters in the password?");
+
+  // Validate that at least one character type has been selected
+  while (!lowercase && !uppercase && !numeric && !special) {
+    alert("Error: At least one character type must be selected. Please try again.");
+    lowercase = confirm("Include lowercase characters in the password?");
+    uppercase = confirm("Include uppercase characters in the password?");
+    numeric = confirm("Include numeric characters in the password?");
+    special = confirm("Include special characters in the password?");
+  }
   
 }
 
@@ -100,6 +123,20 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
+  // Create an array, selectedCharCodes, that will be used to generate password
+  var selectedCharCodes = [];
+  if (lowercase) {
+    selectedCharCodes = selectedCharCodes.concat(lowerCasedCharacters);
+  }
+  if (uppercase) {
+    selectedCharCodes = selectedCharCodes.concat(upperCasedCharacters);
+  }
+  if (numeric) {
+    selectedCharCodes = selectedCharCodes.concat(numericCharacters);
+  }
+  if (special) {
+    selectedCharCodes = selectedCharCodes.concat(special);
+  }
 
 }
 
